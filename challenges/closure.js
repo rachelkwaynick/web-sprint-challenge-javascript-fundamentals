699eda25e1cc9698d8abe2deb15f65a6fe770c60
Rachel Waynick <rachelkwaynick@gmail.com>
2020-09-18 08:53:46 -0700

    MVP Done

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,24 @@ myFunction();
 
 // Explanation: 
 
+/* 
+
+  Because the nestedFunction is still within the bounds of myFunction, and thus within the "gates" that myFunction creates.
+
+*/
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num) {
+  let counter = 0;
+
+  for (let i = 0; i <= num; i++) {
+    counter = counter + i
+  }
+
+  return counter;
+}
+
+console.log(summation(4))
